6be92a79d99223b5b95840948b1acefec8aa6e51
Adam Jermyn <adamjermyn@gmail.com>
2021-03-26 09:46:33 -0400

    New constants.

--- const/public/const_def.f90
+++ const/public/const_def.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a74019863ebe3849ff0898f35d9b6f0de4c955392b33856aab70cce6485a5dc
-size 10190
+oid sha256:117b4b06a84809e0b7292fa6e32842fe6ff856d1ec6c38b87a8ca51fd5de7d2c
+size 10602
