c74a4532bb1243bdf8afb02666583aa2f9bd443f
Adam Jermyn <adamjermyn@gmail.com>
2021-03-26 11:33:59 -0400

    Removed unused variable.

--- eos/private/eos_ctrls_io.f90
+++ eos/private/eos_ctrls_io.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64ea7a432775470d7307810af4a64d47fde1c347596e63f1c1d4648452b959d0
-size 12511
+oid sha256:c202051c53bd98ab79468eeece8f5e3788e75cc696d2d0c6bd9b69d81f283a0c
+size 12467
