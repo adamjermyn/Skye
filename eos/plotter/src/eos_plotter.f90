cf28d8729613a7b19292820279cc54653a7d78f0
Adam Jermyn <adamjermyn@gmail.com>
2022-02-25 11:59:33 -0500

    Improved debug code

--- eos/plotter/src/eos_plotter.f90
+++ eos/plotter/src/eos_plotter.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc849510b8147c8b44e0b40d6ace18bf831ff4951dc8da08541ae2ae86560eb0
-size 15628
+oid sha256:1ed30b1c6443aa1dca2bd7617ae32a307f61f08e2064c1eb846ad046fe7904c7
+size 15817
